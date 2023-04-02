1751bf4b638b2eaa7d508c566cca105f0b58d22f
xsitin <m9ckom@gmail.com>
2023-04-02 15:57:44 +0500

    second try

--- solution.py
+++ solution.py
@@ -1,20 +1,19 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import uniform
 
 
 chat_id = 385459798 # Ваш chat ID, не меняйте название переменной
 
 def confidence_interval(confidence_level, p_values):
-    n = len(p_values)
-    z_critical = norm.ppf(1 - (1 - confidence_level) / 2)
-    p_values_sorted = np.sort(p_values)
-    lower_p = p_values_sorted[int(np.floor((n - 1) * (1 - confidence_level) / 2))]
-    upper_p = p_values_sorted[int(np.ceil((n - 1) * (1 - confidence_level) / 2))]
-    lower_b = 0.05
-    upper_b = lower_b + (upper_p - lower_p) / z_critical
-    return [lower_b, upper_b]
+    maximum_likelihood = np.max(p_values)
+    alpha = 1 - confidence_level
+    loc,scale = uniform.fit(p_values)
+    quantile = uniform.ppf(1 - alpha, loc=loc, scale=scale)
+    right_bound = maximum_likelihood + (quantile - maximum_likelihood)
+    return [right_bound, maximum_likelihood]
+
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
