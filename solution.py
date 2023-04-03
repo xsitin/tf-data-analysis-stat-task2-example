02b9a697838cdc1580ea36448c6edf117d6a3a6b
xsitin <m9ckom@gmail.com>
2023-04-03 22:45:30 +0500

    third try

--- solution.py
+++ solution.py
@@ -6,17 +6,16 @@ from scipy.stats import uniform
 
 chat_id = 385459798 # Ваш chat ID, не меняйте название переменной
 
-def confidence_interval(confidence_level, p_values):
-    maximum_likelihood = np.max(p_values)
-    alpha = 1 - confidence_level
-    loc,scale = uniform.fit(p_values)
-    quantile = uniform.ppf(1 - alpha, loc=loc, scale=scale)
-    right_bound = maximum_likelihood + (quantile - maximum_likelihood)
-    return [right_bound, maximum_likelihood]
-
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return confidence_interval(p, x)
+    alpha1 = (1 - p)/2
+    alpha2 = (1+p)/2
+    n = x.size
+    alpha1, alpha2 = pow(alpha1, 1/n), pow(alpha2, 1/n)
+    
+    x_max = x.max()
+    return (x_max - 0.05)/alpha2 + 0.05, \
+           (x_max - 0.05)/alpha1 + 0.05
